ff7317f8300cbe7aeaccf30921126578700fadfd
barttufest <53616491+barttufest@users.noreply.github.com>
2019-08-02 17:13:20 +0300

    init

---
+++ precache-manifest.3990b5e37f3d61b68aa21b5fea933111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97179d5b55ee94af2401882e4763177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2548509a3f1adbbe964c",
+    "url": "/static/css/main.15a1d874.chunk.css"
+  },
+  {
+    "revision": "3e04c379058c27902d22",
+    "url": "/static/js/2.b54a3a8a.chunk.js"
+  },
+  {
+    "revision": "2548509a3f1adbbe964c",
+    "url": "/static/js/main.4204fc6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
